f3031fd2b3565558d07a026d45863d86ddc59fb4
Crist Yang <cr1st4ever@outlook.com>
2025-05-05 19:33:33 +0800

    用cpp实现数组

---
+++ examples/shuzu.cpp
@@ -0,0 +1,113 @@
+// 01_数组 - 定义一个可扩容的数组
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+class Array {
+public:
+	Array(int size = 10) : mCur(0), mCap_(size) {
+		mpArr = new int[mCap_]();
+	}
+	
+	~Array() {
+		delete[]mpArr;
+		mpArr = nullptr;
+	}
+
+public:
+	void pushBack(int val) {
+		//if (mCap_ == mCur) {
+		//	expand(2 * mCap_);
+		//}
+		checkCap();
+		mpArr[mCur] = val;
+		mCur++;
+	}
+	// 末尾增加元素
+	void popBack() {
+		if (mCur > 0) {
+			mCur--;
+		}
+		else {
+			return;
+		}
+	}
+	// 末尾删除元素
+	void insert(int pos, int val) {
+		if (pos < 0 || pos > mCur) {
+			throw "pop in invalid!";
+		}
+		checkCap();
+		for (int i = mCur - 1; i >= pos; i--) {
+			mpArr[i + 1] = mpArr[i];
+		}
+		mpArr[pos] = val;
+		mCur++;
+	}
+	// 按位置增加元素
+	void erase(int pos) {
+		if (pos >= mCur || pos < 0) {
+			return;
+		}
+		for (int i = pos; i < mCur - 1; i++) {
+			mpArr[i] = mpArr[i + 1];
+		}
+		mCur--;
+	}
+	// 根据位置删除
+	int find(int val) {
+		for (int i = 0; i < mCur; i++) {
+			if (mpArr[i] == val) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	// 查询，返回下标
+	// 打印元素
+	void show() {
+		for (int i = 0; i < mCur; i++) {
+			cout << mpArr[i] << "   ";
+		}
+		cout << endl;
+	}
+
+private:
+	// 内部数组扩容
+	void expand(int size) {
+		int* p = new int[size];
+		memcpy(p, mpArr, sizeof(int) * mCap_);
+		delete[]mpArr;
+		mpArr = p;
+		mCap_ = size;
+	}	
+	void checkCap() {
+		if (mCap_ == mCur) {
+			expand(2 * mCap_);
+		}
+		else return;
+	}
+private:
+	int* mpArr; // 堆上开辟的空间
+	int mCap_;	// 数组的容量
+	int mCur;	// 当前元素个数
+};
+
+
+int main(void) {
+	Array arr;
+
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		arr.pushBack(rand() % 100);
+
+	}
+
+	arr.show();
+	
+}
+
